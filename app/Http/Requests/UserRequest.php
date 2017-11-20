38705c34de2dbbf72b3945fe2c1703ca6aaeddaf
vagrant <vagrant@homestead>
2017-11-20 06:46:25 +0000

    user can upload avatar

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,13 +25,16 @@ class UserRequest extends FormRequest
     {
         return [
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
-            'email' => 'required|email',
-            'introduction' => 'max:80',
+            'email'         =>  'required|email',
+            'introduction'  =>  'max:80',
+            'avatar'        =>  'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200,|max:1024',
         ];
     }
     public function messages()
     {
         return [
+            'avatar.max'    =>  '图片不能超过1M',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 200px 以上',
             'name.unique' => '用户名已被占用，请重新填写',
             'name.regex' => '用户名只支持中英文、数字、横杆和下划线。',
             'name.between' => '用户名必须介于 3 - 25 个字符之间。',
